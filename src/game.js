76d0e58a9ce5aee59808cb07dcb62c3847b98c5d
Cordell <cordellbonnieux@gmail.com>
2021-08-25 18:02:31 -0700

    Set up project files and added css loader.

---
+++ src/game.js
@@ -0,0 +1,5 @@
+
+export function gameArea() {
+
+    return "This is the game area"
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import _ from 'lodash'
+import {gameArea} from './game'
+import './style.css'
 
 function component() {
     const element = document.createElement('div')
 
     // Required for Lodash
-    element.innerHTML = _.join(['Hello', ' Dino Runner'])
+    element.innerHTML = _.join(['Hello', ' Dino Runner', gameArea()])
 
     return element
 }
 
 document.body.appendChild(component())
+
