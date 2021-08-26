9222d7863dfdd1ae4eac0543e9a043a820c69987
Cordell <cordellbonnieux@gmail.com>
2021-08-25 20:01:11 -0700

    Jumping animation and controls working properly.

--- src/game.js
+++ src/game.js
@@ -1,8 +1,11 @@
 
+let block = document.getElementById('block')
+
 export function game() {
     let game = createActor('game')
     game.appendChild(createActor('character'))
     game.appendChild(createActor('block'))
+    
 
     return game 
 }
@@ -11,4 +14,20 @@ function createActor(idName) {
     let div = document.createElement('div')
     div.id = idName
     return div
+}
+
+export function jump() {
+    let character = document.getElementById('character')
+    character.classList.add('animate')
+    setTimeout(function(){
+        character.classList.remove('animate')
+    }, 500)
+}
+
+export function disableScroll() {
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        window.onscroll = function() {
+            window.scrollTo(scrollLeft, scrollTop)
+        }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import _ from 'lodash'
-import {game} from './game'
+import {game, jump, disableScroll} from './game'
 import './style.css'
 
 function component() {
     const element = game()
+    document.addEventListener('keyup', event => {
+        if (event.key === 'ArrowUp') {
+            jump()
+            console.log('space pressed')
+        }
+    })
 
     // Required for Lodash
     //element.innerHTML = _.join([game()])
